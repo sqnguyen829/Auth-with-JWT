18afd2e43aaaf3565265935b1a38ef38a6fb0d47
sqnguyen829 <sqnguyen829@gmail.com>
2020-08-24 16:04:28 -0500

    frontend copied from sessions auth code

---
+++ frontend/src/SessionApp.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App'
+
+function SessionApp() {
+  return (
+    <BrowserRouter>
+        <App/>
+    </BrowserRouter>
+  );
+}
+
+export default SessionApp;
